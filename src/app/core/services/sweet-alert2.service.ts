d8597012661c015e863c7e39703e1f153605ae2a
mh-rafi <macbookpro@MacBooks-MacBook-Pro.local>
2018-06-02 17:39:46 +0600

    alertService and css updated

--- src/app/core/services/sweet-alert2.service.ts
+++ src/app/core/services/sweet-alert2.service.ts
@@ -1,100 +1,55 @@
 import { Injectable } from '@angular/core';
-import swal, { SweetAlertArrayOptions, SweetAlertOptions } from 'sweetalert2';
+import swal, { SweetAlertArrayOptions, SweetAlertOptions, SweetAlertResult } from 'sweetalert2';
 import { Observable, from, of } from 'rxjs';
-// const swalWithMatButtons = swal.mixin({
-//   confirmButtonClass: 'btn btn-success',
-//   cancelButtonClass: 'btn btn-danger',
-//   buttonsStyling: false,
-// })
-
-// class AlertOptions implements SweetAlertOptions {
-//   public confirmButtonText;
-//   public cancelButtonText;
-//   public confirmButtonClass;
-//   public cancelButtonClass;
-
-//   constructor(params: SweetAlertOptions = {}) {
-//     this.confirmButtonText = params.confirmButtonText;
-//     this.cancelButtonText = params.cancelButtonText;
-//     this.cancelButtonClass = params.cancelButtonClass;
-//     this.confirmButtonClass = params.confirmButtonClass;
-//   }
-// }
 
 @Injectable({
   providedIn: 'root'
 })
 export class SweetAlert2Service {
-  swalRef;
-  constructor() {
+  
+  constructor() {}
 
-  }
-  swAlart(options): Observable<any> {
+  public alert(options: SweetAlertOptions = {}): Observable<SweetAlertResult> {
     let defaultOptions = {
       buttonsStyling: false,
       cancelButtonText: false,
-      confirmButtonClass: 'mat-raised-button blue color-white swal-button',
+      confirmButtonClass: 'mat-raised-button t-info t-color-white swal-button',
       cancelButtonClass: 'mat-raised-button mat-warn swal-button'
     }
-    options = { ...defaultOptions, ...options };
-    console.log(options)
+    options = <SweetAlertOptions>{ ...defaultOptions, ...options };
+    // console.log(options)
     return from(swal(options));
   }
-  open() {
-    return swal({
-      title: 'Are you sure?',
-      text: 'You will not be able to recover this imaginary file!',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'No, keep it'
-    }).then((result) => {
-      if (result.value) {
-        swal(
-          'Deleted!',
-          'Your imaginary file has been deleted.',
-          'success'
-        )
-      } else if (result.dismiss === swal.DismissReason.cancel) {
-        swal(
-          'Cancelled',
-          'Your imaginary file is safe :)',
-          'error'
-        )
-      }
-    })
-  }
-  public success({ title, text, confirmButtonText } = <any>{}): Observable<any> {
-    return this.swAlart({
-      title: title || 'Success!',
-      text: text || '',
+  
+  public success(options: SweetAlertOptions = {}): Observable<SweetAlertResult> {
+    return this.alert({
+      title: options.title || 'Success!',
+      text: options.text || '',
       type: 'success',
-      confirmButtonText: confirmButtonText || 'Ok'
-    })
-
-    // this.swalRef = this.open();
-    // console.log(this.swalRef)
+      confirmButtonText: options.confirmButtonText || 'Ok'
+    });
   }
 
-  public confirm({ title, text, confirmButtonText, cancelButtonText } = <any>{}) {
-    return this.swAlart({
-      title: title || 'Confirm',
-      text: text || 'Are you sure?',
+  public confirm(options: SweetAlertOptions = {}): Observable<SweetAlertResult> {
+    return this.alert({
+      title: options.title || 'Confirm',
+      text:  options.text || 'Are you sure?',
       type: 'warning',
       showCancelButton: true,
-      confirmButtonText: confirmButtonText || 'Yes',
-      cancelButtonText: cancelButtonText || 'No'
+      confirmButtonText:  options.confirmButtonText || 'Yes',
+      cancelButtonText:  options.cancelButtonText || 'No'
     })
   }
 
-  public error({ title, text, cancelButtonText } = <any>{}) {
-    return this.swAlart({
-      title: title || 'Error!',
-      text: text || 'Something wrong happened.',
+  public error(options: SweetAlertOptions = {}): Observable<SweetAlertResult> {
+    return this.alert({
+      title: options.title || 'Error!',
+      text: options.text || 'Something went wrong.',
       type: 'error',
       showCancelButton: true,
       showConfirmButton: false,
-      cancelButtonText: cancelButtonText || 'Cancel'
+      cancelButtonText: options.cancelButtonText || 'Cancel'
     })
   }
+
 }
